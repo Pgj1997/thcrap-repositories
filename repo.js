c62455dc95396fc2fc0a8d443dbeaff732074eb8
Pgj1997 <33733954+Pgj1997@users.noreply.github.com>
2019-06-30 17:18:04 -0400

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,10 @@
+{
+	"id": "pgj1997",
+	"neighbors": [
+		"https://srv.thpatch.net"
+	],
+	"patches": {
+		"hq_rips": "Replaces a good chunk of the music with high quality rips ",
+		
+	"title": "Pgj1997's Thcrap repositories;"
+}
