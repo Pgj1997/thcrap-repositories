e7ac0f08397496d99b69e7497c41435363391fa2
Pgj1997 <33733954+Pgj1997@users.noreply.github.com>
2019-06-30 17:03:43 -0400

    initial commit

---
+++ hq-rips/patch.js
@@ -0,0 +1,9 @@
+{
+         "dependencies": [
+                 "nmlgc/base_tsa",
+                 "nmlgc/base_tasofro"
+         ]
+} 	"id": "hq_rips",
+	],
+	"title": "Replaces a good chunk of the music with high quality rips"
+}
