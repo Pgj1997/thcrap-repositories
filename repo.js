533c4502cbb9a15f7ed62f81ee60c15ae708fc0f
Pgj1997 <33733954+Pgj1997@users.noreply.github.com>
2019-07-09 12:22:38 -0400

    Added server link, and shortened the title

--- repo.js
+++ repo.js
@@ -1,10 +1,14 @@
 {
+	"contact": "pgj1997@hotmail.com",
 	"id": "pgj1997",
 	"neighbors": [
 		"https://srv.thpatch.net"
 	],
+	"servers:" [
+		"https://raw.githubusercontent.com/Pgj1997/thcrap-repositories/master"
+	],
 	"patches": {
-		"hq_rips": "Replaces a good chunk of the music with high quality rips ",
+		"hq_rips": "Replaces a good chunk of the music with high quality rips",
 		
-	"title": "Pgj1997's Thcrap repositories;"
+	"title": "Pgj1997's Patches;"
 }
