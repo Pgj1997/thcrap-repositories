c028b3c8e4eb0564d63f943c8789088623174f95
Egor <egor@opensrc.club>
2019-08-20 08:10:15 +0300

    Fix repo.js and patch.js

--- hq-rips/patch.js
+++ hq-rips/patch.js
@@ -1,9 +1,11 @@
 {
-         "dependencies": [
-                 "nmlgc/base_tsa",
-                 "nmlgc/base_tasofro"
-         ]
-} 	"id": "hq_rips",
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/base_tasofro"
+	],
+	"id": "hq_rips",
+	"servers": [
+		"https://mirrors.thpatch.net/pgj1997/hq_rips/"
 	],
 	"title": "Replaces a good chunk of the music with high quality rips"
 }
--- repo.js
+++ repo.js
@@ -2,13 +2,13 @@
 	"contact": "pgj1997@hotmail.com",
 	"id": "pgj1997",
 	"neighbors": [
-		"https://srv.thpatch.net"
+		"https://srv.thpatch.net/"
 	],
-	"servers:" [
-		"https://raw.githubusercontent.com/Pgj1997/thcrap-repositories/master"
+	"servers": [
+		"https://mirrors.thpatch.net/pgj1997/"
 	],
 	"patches": {
-		"hq_rips": "Replaces a good chunk of the music with high quality rips",
-		
-	"title": "Pgj1997's Patches";
+		"hq_rips": "Replaces a good chunk of the music with high quality rips"
+	},	
+	"title": "Pgj1997's Patches"
 }
