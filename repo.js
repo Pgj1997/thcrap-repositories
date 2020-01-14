f9cdfe2042b77304ae8e048310368e75cdb01791
Pgj1997 <33733954+Pgj1997@users.noreply.github.com>
2020-01-14 12:52:32 -0500

    Removed the slashes
    I'll assume "#" isn't used for anything in Javascript, because THCrap isn't parsing it otherwise

--- repo.js
+++ repo.js
@@ -9,7 +9,7 @@
 	],
 	"patches": {
 		"hq-rips": "Replaces a good chunk of the music with high quality rips",
-		"doggo": "Keeps the \#DoggoPhoto\ hashtag in TH165"
+		"doggo": "Keeps the #DoggoPhoto hashtag in TH165"
 	},	
 	"title": "Pgj1997's Patches"
 }
