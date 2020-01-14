91bdd5947d6d16fd8ff587bc7b1e8350b743e601
Pgj1997 <33733954+Pgj1997@users.noreply.github.com>
2020-01-14 12:20:38 -0500

    Add files via upload

---
+++ doggo/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_en"
+	],
+	"id": "doggo",
+	"servers": [
+		"https://mirrors.thpatch.net/pgj1997/doggo/"
+	],
+	"title": "Keeps the \#DoggoPhoto\ hashtag in TH165"
+}
